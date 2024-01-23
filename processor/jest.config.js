74c11f27b474eb69c73efcaccbcadc892d673a9c
Marc Geilen <m.c.w.geilen@tue.nl>
2024-01-23 10:00:02 +0100

    added a test

--- processor/jest.config.js
+++ processor/jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-  };
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    // Added following the suggestion to fix jest issue with d3 on:
+    // https://stackoverflow.com/questions/69226759/jest-unexpected-token-export-when-using-d3
+    '^d3$': '<rootDir>/node_modules/model-image/node_modules/d3/dist/d3.min.js',
+  },
+};
+
+
--- processor/tests/test.ts
+++ processor/tests/test.ts
@@ -1,4 +1,18 @@
-test('some test', () => {
-  const result = 5;
-  expect(result).toBe(5);
+import { expect, test } from '@jest/globals'
+import * as fs from "fs"
+import { makeFromJson } from "../src/make-figures"
+import { DOMConfig } from 'model-image';
+import { JSDOM } from "jsdom"
+
+DOMConfig.DomParser = () => {
+    const dp = (new JSDOM()).window.DOMParser
+    return new dp()
+}
+
+test('test makeFromJson', () => {
+    const inputFile = "./tests/models/vector-with-ruler.json"
+    const outputFile = "./tests/output/vector-with-ruler.svg"
+    const referenceFile = "./tests/output/reference/vector-with-ruler.svg"
+    makeFromJson(inputFile, outputFile)
+    expect(fs.readFileSync(outputFile)).toEqual(fs.readFileSync(referenceFile));
 });
